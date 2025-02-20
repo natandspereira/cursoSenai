58b9a3dfbc4fa11cfc823e59f98372e4a56d24c8
alunosuper <alunosuper@SENAI.LABORATORIOS>
2025-02-20 17:17:21 -0300

    Projeto Land Page Academia

---
+++ landPage/src/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- GOOGLE FONTS -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link 
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,
+        100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" 
+        rel="stylesheet">
+    <!-- GOOGLE FONTS -->
+
+    <!-- LINK CSS -->
+        <link rel="stylesheet" href="./css/style.css">
+    <!-- LINK CSS -->
+
+    <!-- LINK BOOTSTRAP -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+        crossorigin="anonymous">
+    <!-- LINK BOOTSTRAP -->
+    
+    <!-- SCRIPT BOOTSTRAP -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+            crossorigin="anonymous" defer> 
+        </script>
+    <!-- SCRIPT BOOTSTRAP -->
+    <title>Página Academia</title>
+</head>
+
+<body>
+    <!-- HEADER -->
+    <header>
+        <div class="divLogo">
+            <h6>Energy Gym</h6>
+        </div>
+
+        <ul>
+            <li>Home</li>
+            <li>Aluno</li>
+            <li>Contato</li>
+        </ul>
+    </header>
+    <!-- HEADER -->
+    
+    <!-- CAROUSEL BOOTSTRAP  -->
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="./img/img1.jpg" class="d-block w-100" alt="img1">
+            </div>
+            <div class="carousel-item">
+            <img src="./img/img2.jpg" class="d-block w-100" alt="img2">
+            </div>
+            <div class="carousel-item">
+            <img src="./img/img3.jpg" class="d-block w-100" alt="img3">
+            </div>
+        </div>
+    </div>
+    <!-- CAROUSEL  -->
+
+    <!-- FOOTER -->
+        <footer>
+            <h6>Energy Gym</h6>
+
+            <hr>
+            
+            <div class="divSocialMedia">
+                Siga a Energy Gym
+            </div>
+        </footer>
+    <!-- FOOTER -->
+</body>
+
+</html>
