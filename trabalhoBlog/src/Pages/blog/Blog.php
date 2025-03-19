4ff28023fdaa290d1988a11e23c11dbe662bfca7
alunosuper <alunosuper@SENAI.LABORATORIOS>
2025-03-19 17:14:04 -0300

    Trabalho Senai

---
+++ trabalhoBlog/src/Pages/blog/Blog.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- LINK BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/css/bootstrap.min.css" 
+    rel="stylesheet" 
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+    crossorigin="anonymous">
+    <!-- SCRIPT BOOTSTRAP -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+    crossorigin="anonymous" defer>
+    </script>
+    <!-- LINK CSS -->
+    <link rel="stylesheet" href="../../assets/css/blog/Blog.css">
+    <title>Login</title>
+    <title>Blog</title>
+</head>
+<body>
+<!-- HEADER -->
+<header>
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Active</a>
+        </li>
+
+        <li class="nav-item">
+        <a class="nav-link" href="#">Blog</a>
+        </li>
+        
+        <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+        </li>
+    </ul>
+</header>
+            
+<!-- MAIN -->
+<main>
+    <img src="../../assets/img/hacker.jpg" class="img-fluid" alt="hacker">
+</main>
+    
+</body>
+</html>
---
+++ trabalhoBlog/src/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- LINK BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/css/bootstrap.min.css" 
+    rel="stylesheet" 
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+    crossorigin="anonymous">
+    <!-- SCRIPT BOOTSTRAP -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+    crossorigin="anonymous" defer>
+    </script>
+    <!-- LINK CSS -->
+    <link rel="stylesheet" href="../src/assets/css/index/Index.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+        <main>
+            <div class="login">
+                <h3>Blog</h3>
+                <input type="text" placeholder="User">
+                <input type="text" placeholder="Password">
+                <a href="../src/Pages/blog/Blog.php">
+                    <input type="submit" value="Login">
+                </a>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
