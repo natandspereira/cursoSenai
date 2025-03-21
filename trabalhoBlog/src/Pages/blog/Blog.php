57f580a6b2654f2b3f20de4a22fdcb80ee05cd5e
Natan Pereira <contato.natanpereira@gmail.com>
2025-03-21 00:53:06 -0300

    Trabalho Senai

--- trabalhoBlog/src/Pages/blog/Blog.php
+++ trabalhoBlog/src/Pages/blog/Blog.php
@@ -6,18 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- LINK BOOTSTRAP -->
-<<<<<<< HEAD
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/css/bootstrap.min.css" 
     rel="stylesheet" 
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
     crossorigin="anonymous">
 
-=======
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-        crossorigin="anonymous">
->>>>>>> 037be0f3d1bea365e969d929313e4081e18c7035
     <!-- SCRIPT BOOTSTRAP -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
@@ -26,76 +19,116 @@
 
     <!-- LINK CSS -->
     <link rel="stylesheet" href="../../assets/css/blog/Blog.css">
-<<<<<<< HEAD
 
     <title>Login</title>
-=======
->>>>>>> 037be0f3d1bea365e969d929313e4081e18c7035
     <title>Blog</title>
 </head>
 
 <body>
 <!-- HEADER -->
-<<<<<<< HEAD
 <header class="header">
-    <ul class="nav justify-content-center ul">
-        <li class="nav-item li">
-        <a class="nav-link active a" aria-current="page" href="#">Active</a>
-        </li>
-
-        <li class="nav-item li">
-        <a class="nav-link a" href="#">Blog</a>
-        </li>
-        
-        <li class="nav-item li">
-        <a class="nav-link a" href="#">Link</a>
-=======
-<header>
-    <ul class="nav justify-content-center">
-        <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Active</a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="#">Blog</a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
->>>>>>> 037be0f3d1bea365e969d929313e4081e18c7035
-        </li>
-    </ul>
+    <h5 class="h5">Natan Pereira</h5>
 </header>
 
 <!-- MAIN -->
 <main>
-<<<<<<< HEAD
 <!-- DESCRIPTION -->
 <section class="description">
     <div class="ti">
-        <h4>Blog sobre TIC</h4>
+        <img src="../../assets/img/fotoNatan.jpg" class="img-fluid" alt="fotoNatan">
     </div>
 </section>
 <!-- CONTEÚDO -->
-<section class="content">
-    <div class="titleHardware">
-        <h5 class="h5">HARDWARE</h5>
-    </div>
-    
-    <div class="hardware">
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<section class="name">
+    <span class="span">Desenvolvedor Back End</span>
+    <p>Buscando sempre novos desafios e oportunidades para crescer no mundo da tecnologia!</p>
+</section>
+<!-- BTN CV -->
+<div class="btnCV">
+    <a href="https://github.com/natandspereira/meuCurriculo/blob/main/Natan%20Pereira%20-%20Programador%20Back%20End.pdf">
+        <button>Download CV</button>
+    </a>
+</div> 
+<!-- TECNOLOGIAS -->
+ <section class="technology">
+    <h5 class="titleability">Habilidades</h5>
+    <div class="iconTechnology">
+        <div class="row">
+        <div class="tech-item">
+            <label for="html">HTML</label>
+            <img src="../../assets/img/tecnologias/html.svg" alt="HTML">
+        </div>
+        <div class="tech-item">
+            <label for="css">CSS</label>
+            <img src="../../assets/img/tecnologias/css.svg" alt="CSS">
+        </div>
+        <div class="tech-item">
+            <label for="javaScript">JS</label>
+            <img src="../../assets/img/tecnologias/js.svg" alt="JavaScript">
+        </div>
+        <div class="tech-item">
+            <label for="nodeJS">NODEJS</label>
+            <img src="../../assets/img/tecnologias/nodeJS.svg" alt="NodeJS">
+        </div>
+        </div>
+        <div class="row">
+        <div class="tech-item">
+            <label for="github">GITHUB</label>
+            <img src="../../assets/img/tecnologias/github.svg" alt="GitHub">
+        </div>
+        <div class="tech-item">
+            <label for="linux">LINUX</label>
+            <img src="../../assets/img/tecnologias/linux.svg" alt="Linux">
+        </div>
+        <div class="tech-item">
+            <label for="windows">WINDOWS</label>
+            <img src="../../assets/img/tecnologias/windows.svg" alt="Windows">
+        </div>
+        <div class="tech-item">
+            <label for="reactJS">REACTJS</label>
+            <img src="../../assets/img/tecnologias/reactJS.svg" alt="ReactJS">
+        </div>
+        </div>
+        <div class="row">
+        <div class="tech-item">
+            <label for="git">GIT</label>
+            <img src="../../assets/img/tecnologias/git.svg" alt="Git">
+        </div>
+        <div class="tech-item">
+            <label for="vscode">VSCODE</label>
+            <img src="../../assets/img/tecnologias/vscode.svg" alt="VSCode">
+        </div>
+        <div class="tech-item">
+            <label for="mysql">MYSQL</label>
+            <img src="../../assets/img/tecnologias/mysql.svg" alt="MySQL">
+        </div>
+        <div class="tech-item">
+            <label for="typeScript">TYPESCRIPT</label>
+            <img src="../../assets/img/tecnologias/typeScript.svg" alt="TypeScript">
+        </div>
         </div>
     </div>
-</section> 
-</main>
-=======
->>>>>>> 037be0f3d1bea365e969d929313e4081e18c7035
+
+        
     
+ </section>
+<!-- CONTATO -->
+<div class="contact">
+    <div class="titleContact">
+        <h5>Contato</h5><br>
+    </div>
+    <div class="imgContact">
+    <a class="nav-link active a" aria-current="page" href="https://www.linkedin.com/in/natandspereira">
+        <img src="../../assets/img/redesSociais/linkedin.svg" class="img-fluid imgSocial" alt="linkedin">
+    </a>
+    <a class="nav-link active a" aria-current="page" href="https://github.com/natandspereira ">
+        <img src="../../assets/img/redesSociais/github.svg" class="img-fluid imgSocial" alt="github">
+    </a>
+    </div>
+</div>
 </main>
+
+
 
 <!-- FOOTER -->
 <footer></footer>
