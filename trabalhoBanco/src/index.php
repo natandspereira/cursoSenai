93d0a810b03f65d8194e9480ab77240026146f8d
alunosuper <alunosuper@SENAI.LABORATORIOS>
2025-03-25 17:10:14 -0300

    Trabalho Senai

---
+++ trabalhoBanco/src/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- LINK BOOTSTRAP -->
+
+    <!-- SCRIPT BOOTSTRAP  -->
+    
+    <!-- LINK CSS -->
+    <link rel="stylesheet" href="./assets/css/Index.css">
+    
+    <!-- TITLE -->
+    <title>Bank Login</title>
+</head>
+<body>
+    <main>
+        <div class="logo">
+            <img src="./assets/img/sicrediLogo.png" alt="sicrediLogo">
+        </div>
+        <div class="container">
+            <h4>Bank</h4>
+            <input type="text" placeholder="CPF">
+            <input type="text" placeholder="Senha">
+            <a href="./pages/Bank.php">
+                <input type="submit" value="Entrar">
+            </a>
+        </div>
+    </main>
+</body>
+</html>
---
+++ trabalhoBanco/src/pages/Bank.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- LINK BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+    crossorigin="anonymous">
+
+    <!-- SCRIPT BOOTSTRAP -->
+    <script 
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+    crossorigin="anonymous" defer>
+    </script>
+
+    <!-- LINK CSS -->
+    <link rel="stylesheet" href="../assets/css/Bank.css">
+    
+    <!-- LINK ICON GOOGLE -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=account_circle" />
+     <!-- TITLE -->
+    <title>Bank</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar bg-body-tertiary">
+        <img src="../assets/img/sicrediLogo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                
+                    <span class="material-symbols-outlined">
+                        account_circle
+                    </span>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            assss
+        </div>
+    </main>
+</body>
+</html>
